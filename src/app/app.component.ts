7328dc3a21d6ebad1830e7d824619820891794f0
Edgardo Molina <126525573+edgardo-molina-gonzalez@users.noreply.github.com>
2023-11-01 16:16:24 -0300

    Estructura del proyecto finalizada.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { HomeComponent } from './gifs/pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SidebarComponent, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'gifs-app';
